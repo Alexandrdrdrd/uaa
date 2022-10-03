9dd144b92e10a5296ce21451b1381b7559b80fe2
Oleksandr Avdieiev <oleksandr.avdieiev@uniqa.ua>
2022-10-03 16:27:00 +0300

    first commit

--- src/main/java/com/sasha/uaa/app/security/DomainUserDetailsService.java
+++ src/main/java/com/sasha/uaa/app/security/DomainUserDetailsService.java
@@ -34,7 +34,6 @@ public class DomainUserDetailsService implements UserDetailsService {
     @Transactional
     public UserDetails loadUserByUsername(final String login) {
         log.debug("Authenticating {}", login);
-
         if (new EmailValidator().isValid(login, null)) {
             return userRepository.findOneWithAuthoritiesByEmailIgnoreCase(login)
                 .map(user -> createSpringSecurityUser(login, user))
